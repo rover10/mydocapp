432cb15d1c3e3e806e0ede84d78976899ee3577f
rakesh.kumar <rakesh.kumar@eagleview.com>
2020-07-26 14:26:32 +0530

    Read all appointments

--- server/read.go
+++ server/read.go
@@ -1,7 +1,12 @@
 package server
 
 import (
+	"fmt"
+	"net/http"
+
 	"github.com/labstack/echo"
+	"github.com/rover10/mydocapp.git/model"
+	"github.com/rover10/mydocapp.git/token"
 )
 
 //Doctor read a doctor
@@ -16,8 +21,16 @@ func Clinic(context echo.Context) error {
 
 //Appointment read appointment
 func (s *Server) Appointment(context echo.Context) error {
-	s.DB.RetrieveAppointment("sid")
-	return nil
+	login := token.GetLoggedIn(context)
+	accountID := login["uid"].(string)
+	fmt.Println(accountID)
+	appointments := []model.Appointment{}
+	if err := s.DB.DBORM.Table("appointment").Where("account_id = ?", accountID).Find(&appointments).Error; err != nil {
+		return err
+	}
+	return context.JSON(http.StatusOK, appointments)
+	//s.DB.RetrieveAppointment(sid)
+
 }
 
 //Treatment read treatment detail which includes patient_problem_description, prescription, test
