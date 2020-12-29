70f76ff000fab3550ee82efdd9adbb7c1e27364c
rakesh.kumar <rakesh.kumar@eagleview.com>
2020-12-29 21:24:11 +0530

    Added root api

--- api/api.go
+++ api/api.go
@@ -44,7 +44,7 @@ func Api(server *server.Server) {
 	server.Router.GET(path.Join(server.APIPath, "/v1/clinics"), server.Clinic, auth.IsLoggedIn)
 	server.Router.GET(path.Join(server.APIPath, "/v1/qrcode"), server.GenerateQr, auth.IsLoggedIn)
 	server.Router.GET(path.Join(server.APIPath, "/v1/search"), server.Search, auth.IsLoggedIn)
-	server.Router.File("/", "app/index.html")
+	//server.Router.File("/", "app/index.html")
 	server.Router.Static("static/*", "web/assets")
 	//e.Static("/", "assets")
 	server.Router.GET(path.Join(server.APIPath, "home"), server.Home)
