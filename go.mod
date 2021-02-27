c34192c8e662413887fb5bedba3998fd8f9c5b5a
rakesh.kumar <rakesh.kumar@eagleview.com>
2021-02-28 01:42:29 +0530

    Added index

--- go.mod
+++ go.mod
@@ -1,3 +1,14 @@
 module github.com/rover10
 
 go 1.14
+
+require (
+	github.com/blevesearch/bleve v1.0.14
+	github.com/boombuler/barcode v1.0.1
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
+	github.com/jinzhu/gorm v1.9.16
+	github.com/labstack/echo v3.3.10+incompatible
+	github.com/labstack/gommon v0.3.0
+	github.com/lib/pq v1.9.0 // indirect
+	github.com/satori/go.uuid v1.2.0
+)
---
+++ server/index.go
@@ -0,0 +1,65 @@
+package server
+
+import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/rover10/model"
+)
+
+// Index doctor
+// On create & update
+// Index hospitals/clinic
+// On create & update
+type IndexService interface {
+	IndexDoctor(model.Doctor) error
+}
+
+type IndexServiceImpl struct {
+	Client *http.Client
+}
+
+func (indexService *IndexServiceImpl) IndexDoctor(dr model.Doctor) error {
+	// Get id
+
+	// Get related fields
+	// Prepare json body
+	// Index
+	indexDrUrl := "https://aw8akjyvyt:tyqzkp81x7@tarkol-8348260269.eu-central-1.bonsaisearch.net:443/doctors/_doc/" + dr.AccountID.String()
+	//indexService.Client.Post()
+	var timeout time.Duration = time.Second * 3
+	//var resbody []byte
+	client := &http.Client{
+		Timeout: timeout,
+	}
+
+	reqBody, err := json.Marshal(dr)
+	if err != nil {
+		log.Printf("Error marshalling payload to json %v", err)
+		return errors.New("Error marshaling payload")
+	}
+
+	endpoint := indexDrUrl
+	request, _ := http.NewRequest("POST", endpoint, bytes.NewReader(reqBody))
+	request.Header.Set("Content-Type", "application/json")
+	response, err := client.Do(request)
+	if err != nil {
+		errStr := fmt.Sprintf("Error connecting to server %v", err)
+		log.Print(errStr)
+		return errors.New(errStr)
+	}
+	defer response.Body.Close()
+	_, err = ioutil.ReadAll(response.Body)
+
+	return nil
+
+}
+
+// Search indexed doctor by disease & city
+// Search clinic by name & city
